c6b01c74886af5d6c9810080705fa69c5e954bfe
safdarsidhik <safdarsidhik@gmail.com>
2025-11-17 17:46:12 +0530

    improved the UI

--- GDP Prediction/app.py
+++ GDP Prediction/app.py
@@ -9,24 +9,30 @@ model = joblib.load("random_forest_model.pkl")
 
 @app.route("/")
 def home():
+    return render_template("welcome.html")
+
+@app.route("/predictform")
+def form_page():
     return render_template("index.html")
+
+@app.route("/welcome")
+def welcome():
+    return render_template("welcome.html")
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        # ----- INPUTS EXACTLY LIKE DATASET -----
-        exports_pct = float(request.form["exports_pct"])              # %
-        imports_pct = float(request.form["imports_pct"])              # %
-        population = float(request.form["population"])                # integer, NOT billions
-        fdi_pct = float(request.form["fdi_pct"])                      # %
-        govt_exp_pct = float(request.form["govt_exp_pct"])            # %
-        oil_rents_pct = float(request.form["oil_rents_pct"])          # %
-        exchange_rate = float(request.form["exchange_rate"])          # LCU per USD
+        exports_pct = float(request.form["exports_pct"])
+        imports_pct = float(request.form["imports_pct"])
+        population = float(request.form["population"])
+        fdi_pct = float(request.form["fdi_pct"])
+        govt_exp_pct = float(request.form["govt_exp_pct"])
+        oil_rents_pct = float(request.form["oil_rents_pct"])
+        exchange_rate = float(request.form["exchange_rate"])
 
     except ValueError:
         return "Invalid input. Please enter valid numbers."
 
-    # ---- Create feature vector in EXACT ORDER USED IN TRAINING ----
     features = np.array([[ 
         exports_pct,
         imports_pct,
@@ -37,10 +43,7 @@ def predict():
         exchange_rate
     ]])
 
-    # ---- Model prediction (log-scale) ----
     log_prediction = model.predict(features)[0]
-
-    # ---- Convert back from log to BILLIONS USD ----
     gdp_billion = np.expm1(log_prediction)
 
     return render_template("result.html", prediction=round(gdp_billion, 3))
